0b5c80ac33ef04a53b98b5514c8c1fc13a766091
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2023-07-09 15:42:59 +0000

    管理者用商品画面制作開始

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
@@ -7,18 +8,24 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item.params)
+    item = Item.new(item_params)
     item.save
-    redirect_to 'item/index'
+    redirect_to item_path(item.id)
   end
 
   def show
+   @item = Item.find(params[:id])
   end
 
   def edit
+   @item = Item.find(params[:id])
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_items_path
+
   end
 
   private
