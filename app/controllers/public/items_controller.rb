285b83283d2572b1f92e8f8c4e0c543e5e283cb4
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2023-07-10 14:43:33 +0000

    顧客用商品一覧作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,10 @@
 class Public::ItemsController < ApplicationController
   def index
+    @itemcount = Item.count
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end
